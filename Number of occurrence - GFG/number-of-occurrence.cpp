ee5a574c2b36031b75573b326e1b0e962796cfe2
Pranshu Adhwaryu <95425577+Pranshh19@users.noreply.github.com>
2023-08-20 22:36:29 +0530

    Added solution - LeetHub

--- Number of occurrence - GFG/number-of-occurrence.cpp
+++ Number of occurrence - GFG/number-of-occurrence.cpp
@@ -10,18 +10,91 @@ class Solution{
 public:	
 	/* if x is present in arr[] then returns the count
 		of occurrences of x, otherwise returns 0. */
+    bool isPossible(int arr[], int n, int x){
+        
+        int start = 0;
+        int end = n-1;
+        int mid = start+(end-start)/2;
+        
+        while(start<=end){
+            if(arr[mid]==x){
+                return true;
+            }
+            else if(arr[mid]>x){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+            mid = start+(end-start)/2;
+        }
+        
+        return false;
+    }
+	
+	int upper(int arr[], int n, int x){
+        int start = 0;
+        int end = n-1;
+        int mid = start+(end-start)/2;
+        int ans = 0;
+        while(start<=end){
+            if(arr[mid]==x){
+                ans = mid;
+                start = mid+1;
+            }
+            else if(arr[mid]>x){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+             mid = start+(end-start)/2;
+        }
+        
+        return ans;
+	}
+	
+	int lower(int arr[], int n, int x){
+	    int start = 0;
+        int end = n-1;
+        int mid = start+(end-start)/2;
+        int ans = 0;
+        while(start<=end){
+            if(arr[mid]==x){
+                ans = mid;
+                end = mid-1;
+            }
+            else if(arr[mid]>x){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+             mid = start+(end-start)/2;
+        }
+        
+        return ans;
+	}
+		
 	int count(int arr[], int n, int x) {
-	    // code here
-	    sort(arr,arr+n);
-	    
-	    int count = 0;
-	    
-	    for(int i =0; i<n; i++){
-	        if(arr[i]==x){
-	            count++;
-	        }
-	    }
-	    return count;
+    
+        //Step1=Check is element is present
+        //Step2 Check upper Bound
+        //Step3: Check Lower Bound
+        
+        
+        if(isPossible(arr,n,x)){
+            //Check UpperBound
+            int ub = upper(arr,n,x);
+            
+            //check LoweBound
+            int lb = lower(arr,n,x);
+            
+            return ub-lb+1;
+        }	
+        
+        return 0;
+
 	}
 };
 
